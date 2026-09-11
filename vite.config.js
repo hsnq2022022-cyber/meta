e6f46dd9d1c9570284fbad87d26bcfdd8f79adb5
hsnq2022022-cyber <hsnq2022022@gmail.com>
2026-09-11 23:14:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-```ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -17,4 +16,3 @@ export default defineConfig({
     },
   },
 });
-```
